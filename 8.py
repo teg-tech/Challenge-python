62263fdb8a144d57a1bfb6e08c9c5a2ae7e9c1b7
teg-tech <vgyokeyeres@gmail.com>
2026-07-19 20:39:52 +0700

    menyelesaikan challenge 8

---
+++ 8.py
@@ -0,0 +1,21 @@
+saldo = 0
+
+
+
+for i in range(1,6):
+    tabungan = 1 * 1000
+    saldo = saldo + tabungan
+    print("hari", i)
+    print("menabung", tabungan)
+    print("saldo", saldo)
+    print()
+
+# saldo = 0
+
+# for i in range(1, 6):
+#     tabungan = i * 1000
+#     saldo = saldo + tabungan
+#     print("Hari", i)
+#     print("Menabung :", tabungan)
+#     print("Saldo    :", saldo)
+#     print()
